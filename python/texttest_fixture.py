702b19ad4b99c61033befe8f3960df8dd8df3c4c
Wesley Blake <wesley@nikordi.com>
2017-04-20 22:29:04 +0200

    fix pep8 violations

--- python/texttest_fixture.py
+++ python/texttest_fixture.py
@@ -11,9 +11,15 @@ if __name__ == "__main__":
              Item(name="Elixir of the Mongoose", sell_in=5, quality=7),
              Item(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
              Item(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
-             Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20),
-             Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=49),
-             Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=49),
+             Item(name="Backstage passes to a TAFKAL80ETC concert",
+                  sell_in=15,
+                  quality=20),
+             Item(name="Backstage passes to a TAFKAL80ETC concert",
+                  sell_in=10,
+                  quality=49),
+             Item(name="Backstage passes to a TAFKAL80ETC concert",
+                  sell_in=5,
+                  quality=49),
              Item(name="Conjured Mana Cake", sell_in=3, quality=6),  # <-- :O
             ]
 
